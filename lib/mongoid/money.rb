4d6446e2568e0b382ba61d677600d495e13a656a
Scott Raio <scottraio@gmail.com>
2015-01-15 21:44:50 -0800

    stack level too deep, so changing location of money file

--- lib/mongoid/money.rb
+++ lib/mongoid/money.rb
@@ -3,3 +3,9 @@ require 'monetize'
 require 'monetize/core_extensions'
 require 'mongoid/money/version'
 require 'mongoid/money/configuration'
+
+module Mongoid
+  module Money
+    extend Configuration
+  end
+end
